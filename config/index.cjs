2b965a88b55a294f8a789a3f9909f1bc1e8ba781
x135x <114560448+x135x@users.noreply.github.com>
2022-09-28 15:13:22 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,55 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wxdee9bf8a710983f7',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
-
-  // 是否给文字设置多彩颜色, 和emoji不兼容
-  // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
-  IS_SHOW_COLOR: true,
-
-  // 功能开关,打开：true，关闭：false
-  SWITCH: {
-    /** 每日天气 */
-    weather: true,
-
-    /** 节假日 */
-    // 下一休息日综合提醒
-    holidaytts: true,
-
-    /** 每日N句 */
-    // 金山每日一句
-    CIBA: true,
-    // 每日一言
-    oneTalk: false,
-    // 土味情话(彩虹屁)
-    earthyLoveWords: false,
-    // 朋友圈文案
-    momentCopyrighting: false,
-    // 毒鸡汤
-    poisonChickenSoup: false,
-    // 古诗古文
-    poetry: false,
-
-    /** 星座运势 */
-    horoscope: false,
-
-    /** 生日消息和节日消息 */
-    birthdayMessage: true,
-
-    // 学生课表
-    courseSchedule: false,
-  },
-
-  /** 每日一言 */
-
-  // 每日一言的内容类型
-  // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-  LITERARY_PREFERENCE: '',
+  APP_SECRET: 'e226d2fbbfa0abd6f836656efa74399b',
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
@@ -63,11 +20,11 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oTYJg5-MHJtlh3Ki6O1qY3CG3A-o',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'pcMVWZAIUUN5yUL4TjCti5RpTKZhwXJcw2J5ARI8dz4',
       // 所在省份
       province: '广东',
       // 所在城市
